f34a91833a7466b76c5c073955cbf2aabe02c0ae
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-06-16 15:56:12 +0900

    add '/api' to all paths

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const router = require('./router');
 
-mongoose.connect('mongodb://localhost/borda');
+mongoose.connect('mongodb://127.0.0.1:27017/borda');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +12,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use('/events', router.event);
+app.use('/api/events', router.event);
 
 app.listen(8080);
